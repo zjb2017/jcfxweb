5ee7ac330766a7605c19b9a5a9b5a58f644c1ab3
zjb2017 <exnet@vip.qq.com>
2017-05-20 09:28:51 +0800

    完善了登录界面

--- routes/login.js
+++ routes/login.js
@@ -3,24 +3,40 @@ var router = express.Router();
 
 /* GET home page. */
 router.post('/', function (req, res, next) {
+  var r = {};
+
   var t = req.query['t'];
   switch (t) {
     case 'login': {
       if (req.body.login == 'admin' && req.body.password == '123456') {
+        //设置Session，保存登录状态
         req.session.user_id = 'admin';
-        req.session.user_name='admin';
-        res.send('ok');
+        req.session.user_name = 'admin';
+        //发送数据给客户端
+
+
+        r.success = 'success';            //业务层成功标示(success/failed)
+        r.errCode = 0;
+        r.msg = '登录成功';
       } else {
-        res.send('Invalid Parameter');
+        r.success = 'failed';            //业务层成功标示(success/failed)
+        r.errCode = 1;
+        r.msg = '登录失败：用户名密码错误';
       }
 
       break;
     }
     default:
       {
+        r.success = 'failed';            //业务层成功标示(success/failed)
+        r.errCode = 500;
+        r.msg = 'Invalid Parameter';
         break;
       }
   }
+
+  var resdata = JSON.stringify(r);
+  res.send(resdata);
 
 });
 
