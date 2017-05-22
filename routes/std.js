c936dad04d5b05e08a63824ca59aa0e9d49fc4b4
zjb2017 <exnet@vip.qq.com>
2017-05-22 10:58:02 +0800

    修改了示例代码.

--- routes/std.js
+++ routes/std.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+//json转换为字符串,用在POST提交参数用
 var querystring = require('querystring');
-//json转换为字符串
 
 
 var jcblsrv = require('../jcblsrv');
@@ -10,15 +10,17 @@ var jcblsrv = require('../jcblsrv');
 
 /* GET home page. */
 router.post('/', function (req, res, next) {
-    var r = {};
+
 
     var t = req.query['t'];
     switch (t) {
         case 'add': {
+            //获取WEB前端提交过来的POST参数
             var xh = req.body.xh;
             var xm = req.body.xm;
             var xb = req.body.xb;
             var pwd = req.body.pwd;
+
 
             //将WEB前端提交来的数据打包发送给【数据层】进行处理
             var postData = querystring.stringify({
@@ -28,36 +30,52 @@ router.post('/', function (req, res, next) {
                 PWD: pwd
             });
 
-            var act='user.sql.add.stduent'
-            jcblsrv.DBRequest(act,postData, function (chunk) {
+            //数据层接口调用
+            var act = 'user.sql.add.stduent';
+            jcblsrv.DBRequest(act, postData, function (chunk) {
                 //将处理完毕的数据返回WEB客户端
                 res.send(chunk);
             });
+            break;
+        }
+        case 'cx': {
+            //获取WEB前端提交过来的POST参数
+            var page = req.body.page;
+            var rows = req.body.rows;
+            var itemid = req.body.itemid;
+            var productid = req.body.productid;
+            //将WEB前端提交来的数据打包发送给【数据层】进行处理
+            var postData = querystring.stringify({
 
-            /* if (req.body.login == 'admin' && req.body.password == '123456') {
-               //设置Session，保存登录状态
-               req.session.user_id = 'admin';
-               req.session.user_name = 'admin';
-               //发送数据给客户端
-       
-       
-               r.success = 'success';            //业务层成功标示(success/failed)
-               r.errCode = 0;
-               r.msg = '登录成功';*/
-            // } else {
-            r.success = 'failed';            //业务层成功标示(success/failed)
-            r.errCode = 1;
-            r.msg = '数据添加成功';
-            //}
+            });
+            var act = 'user.sql.query.stduent'
+            jcblsrv.DBRequest(act, postData, function (chunk) {
+                //将处理完毕的数据返回WEB客户端，由于前端WEB采用了DataGrid表格对象，所以对返回值进行一个处理，以满足其格式
+                //res.send(chunk);
+                var d = JSON.parse(chunk);
+                if (d.success == 'true' && d.errCode == '0') {
+                    //数据层查询成功
+                    var rows = d.result;
+                    var total = d.totalCount;
+                    var data = JSON.stringify({
+                        total: total,
+                        rows: rows
+                    });
+                    res.send(data);
+                } else {
+                    //数据层查询失败,返回空数据
+                    res.send('{"total":0,"rows":[]}');
+                }
+            });
 
             break;
         }
         default:
             {
-                r.success = 'failed';            //业务层成功标示(success/failed)
+                var r = {};
+                r.success = 'false';            
                 r.errCode = 500;
                 r.msg = 'Invalid Parameter';
-
                 var resdata = JSON.stringify(r);
                 res.send(resdata);
 
